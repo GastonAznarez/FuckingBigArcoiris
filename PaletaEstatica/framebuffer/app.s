ac9a9a88e76dc5c78136c1a3aa87647f9b3c1e05
GastonAznarez <gastonaznarez@gmail.com>
2018-05-31 12:58:38 -0300

    Arreglo de tabulaciones de 2 espacios

--- PaletaEstatica/framebuffer/app.s
+++ PaletaEstatica/framebuffer/app.s
@@ -1,9 +1,9 @@
 //----------------------- 16/5/18 -----------------------//
-// Integrantes: 																				 //
-//							Aznarez Gaston													 //
-//							Guerra Lucas														 //
-//							Lauret Ignacio													 //
-//							Legnini Papalardo Gino									 //
+// Integrantes: 										 //
+//							Aznarez Gaston				 //
+//							Guerra Lucas				 //
+//							Lauret Ignacio				 //
+//							Legnini Papalardo Gino		 //
 //-------------------------------------------------------//
 
 
@@ -45,12 +45,12 @@ app:
 
 		bl bajarRojo
 
-		lsl x1, x1, 1 					//Multiplico la cantidad de X que quedan por dos [Sacar]
-		add x0,x0,x1	 				  //Cambio el pixel seleccionado por X*X mas [Sacar]
+		lsl x1, x1, 1 						//Multiplico la cantidad de X que quedan por dos [Sacar]
+		add x0,x0,x1	 					//Cambio el pixel seleccionado por X*X mas [Sacar]
 
-		sub x2, x2, 1 					//Le resto uno a los Y
+		sub x2, x2, 1 						//Le resto uno a los Y
 
-		cbnz x2, loop1 					//Si me quedan Ys (x2 != 0) vuelvo al loop
+		cbnz x2, loop1 						//Si me quedan Ys (x2 != 0) vuelvo al loop
 
 //--------END LOOP Y----------------------------------------------//
 
@@ -62,41 +62,41 @@ app:
   subirRojo: 	//Termina cuando el color se termina o el X = 0
 
     add w10, w3, w4
-  	add w10, w10, w5   	  //Genero el color R + G + B
+  	add w10, w10, w5   						//Genero el color R + G + B
 
-    sturh w10,[x0]			  //Pinto el primer bit
-		add x0,x0,2 					//Selecciono otro bit
-		sturh w10,[x0]				//Pinto el segundo bit
-    add x0,x0,2	  				//Selecciono otro bit
+    sturh w10,[x0]							//Pinto el primer bit
+	add x0,x0,2 							//Selecciono otro bit
+	sturh w10,[x0]							//Pinto el segundo bit
+    add x0,x0,2	  							//Selecciono otro bit
 
-    sub x1,x1,2	   				// decrement X counter
-    cbz x1,alTreinta	    //Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
+    sub x1,x1,2	   							// decrement X counter
+    cbz x1,alTreinta	 				 	//Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
 
     mov w7, 0xF8
-    lsl w7, w7, 8					//w7 es el valor maximo de R
-    sub w6,w3,w7	  			//R - max R
-    cbz w6, alTreinta	    // Si R es su maximo
+    lsl w7, w7, 8							//w7 es el valor maximo de R
+    sub w6,w3,w7	  						//R - max R
+    cbz w6, alTreinta	    				// Si R es su maximo
 
-    add x3, x3, 0x800 		//Sumo una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
+    add x3, x3, 0x800 						//Sumo una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
 
     b subirRojo  //Llamada recirsiva, todavia no termino el color
 
   bajarRojo:
 
     add w10, w3, w4
-    add w10, w10, w5   	 //Genero el color R + G + B
+    add w10, w10, w5   	 					//Genero el color R + G + B
 
-    sturh w10,[x0] 			 //Pinto el primer bit
-		add x0,x0,2					 //Selecciono otro bit
-		sturh w10,[x0] 			 //Pinto el segundo bit
-    add x0,x0,2	  			 // Selecciono otro bit
+    sturh w10,[x0] 			 				//Pinto el primer bit
+		add x0,x0,2					 		//Selecciono otro bit
+		sturh w10,[x0] 						//Pinto el segundo bit
+    add x0,x0,2	  							// Selecciono otro bit
 
-    sub x1,x1,2	  			 // decrement X counter
-    cbz x1,alTreinta	   //Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
+    sub x1,x1,2	  							// decrement X counter
+    cbz x1,alTreinta	   					//Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
 
-    sub w3, w3, 0x800 	 //Resto una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
+    sub w3, w3, 0x800 	 					//Resto una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
 
-    cbz w3, alTreinta	   // Si R es su minimo termina la funcion (vuelve a la siguiente linea de la que empezo)
+    cbz w3, alTreinta	   					// Si R es su minimo termina la funcion (vuelve a la siguiente linea de la que empezo)
 
     b bajarRojo  //Llamada recirsiva, todavia no termino el color
 
@@ -107,40 +107,40 @@ app:
 	subirAzul: //Termina cuando el color se termina o el X = 0
 
     add w10, w3, w4
-  	add w10, w10, w5    	//Genero el color R + G + B
+  	add w10, w10, w5    					//Genero el color R + G + B
 
-    sturh w10,[x0] 				//Pinto el primer bit
-		add x0,x0,2 					//Selecciono otro bit
-		sturh w10,[x0] 				//Pinto el segundo bit
-    add x0,x0,2	   				//Selecciono otro bit
+    sturh w10,[x0] 							//Pinto el primer bit
+		add x0,x0,2 						//Selecciono otro bit
+		sturh w10,[x0] 						//Pinto el segundo bit
+    add x0,x0,2	   							//Selecciono otro bit
 
-    sub x1,x1,2	  				//Decrement X counter
-    cbz x1,alTreinta	    //Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
+    sub x1,x1,2	  							//Decrement X counter
+    cbz x1,alTreinta	   					//Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
 
-    mov w7, 0x1F					//w7 es el valor maximo de B
-    sub w6,w5,w7	 			  //B - max B
-    cbz w6, alTreinta 	  //Si B es su maximo
+    mov w7, 0x1F							//w7 es el valor maximo de B
+    sub w6,w5,w7	 						//B - max B
+    cbz w6, alTreinta 	 					//Si B es su maximo
 
-    add w5, w5, 1 				//Sumo una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
+    add w5, w5, 1 							//Sumo una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
 
     b subirAzul  //Llamada recirsiva, todavia no termino el color
 
   bajarAzul:
 
     add w10, w3, w4
-    add w10, w10, w5   		//Genero el color R + G + B
+    add w10, w10, w5   						//Genero el color R + G + B
 
-    sturh w10,[x0]				//Pinto el primer bit
-		add x0,x0,2 					//Selecciono otro bit
-		sturh w10,[x0] 				//Pinto el segundo bit
-    add x0,x0,2	   				//Selecciono otro bit
+    sturh w10,[x0]							//Pinto el primer bit
+		add x0,x0,2 						//Selecciono otro bit
+		sturh w10,[x0] 						//Pinto el segundo bit
+    add x0,x0,2	   							//Selecciono otro bit
 
-    sub x1,x1,2	  				//Decrement X counter
-    cbz x1,alTreinta	    //Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
+    sub x1,x1,2	  							//Decrement X counter
+    cbz x1,alTreinta	   					//Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
 
-    sub w5, w5, 1  				//Resto una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
+    sub w5, w5, 1  							//Resto una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
 
-    cbz w5, alTreinta	    // Si B es su minimo termina la funcion (vuelve a la siguiente linea de la que empezo)
+    cbz w5, alTreinta	  					// Si B es su minimo termina la funcion (vuelve a la siguiente linea de la que empezo)
 
     b bajarAzul  //Llamada recirsiva, todavia no termino el color
 
@@ -151,40 +151,40 @@ app:
 	subirVerde: //Termina cuando el color se termina o el X = 0
 
     add w10, w3, w4
-  	add w10, w10, w5    	//Genero el color R + G + B
+  	add w10, w10, w5    					//Genero el color R + G + B
 
-    sturh w10,[x0] 				//Pinto el primer bit
-		add x0,x0,2 					//Selecciono otro bit
-		sturh w10,[x0]				//Pinto el segundo bit
-    add x0,x0,2	   				// Selecciono otro bit
+    sturh w10,[x0] 							//Pinto el primer bit
+	add x0,x0,2 							//Selecciono otro bit
+	sturh w10,[x0]							//Pinto el segundo bit
+    add x0,x0,2	   							// Selecciono otro bit
 
-    sub x1,x1,2	   				// decrement X counter
-    cbz x1,alTreinta	    //Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
+    sub x1,x1,2	   							// decrement X counter
+    cbz x1,alTreinta	   					//Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
 
-    mov w7, 0x7C0					//w7 es el valor maximo de G
-    sub w6,w4,w7	   			//G - max G
-    cbz w6, alTreinta	    // Si G es su maximo
+    mov w7, 0x7C0							//w7 es el valor maximo de G
+    sub w6,w4,w7	   						//G - max G
+    cbz w6, alTreinta	   					// Si G es su maximo
 
-    add w4, w4, 0x40			//Sumo una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
+    add w4, w4, 0x40						//Sumo una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
 
     b subirVerde  //Llamada recirsiva, todavia no termino el color
 
   bajarVerde:
 
     add w10, w3, w4
-    add w10, w10, w5    	//Genero el color R + G + B
+    add w10, w10, w5    					//Genero el color R + G + B
 
-    sturh w10,[x0] 				//Pinto el primer bit
-		add x0,x0,2 					//Selecciono otro bit
-		sturh w10,[x0] 				//Pinto el segundo bit
-    add x0,x0,2	   				// Selecciono otro bit
+    sturh w10,[x0] 							//Pinto el primer bit
+		add x0,x0,2 						//Selecciono otro bit
+		sturh w10,[x0] 						//Pinto el segundo bit
+    add x0,x0,2	   							// Selecciono otro bit
 
-    sub x1,x1,2	   				// decrement X counter
-    cbz x1,alTreinta	    //Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
+    sub x1,x1,2	   							// decrement X counter
+    cbz x1,alTreinta	   					//Si es el ultimo X, terminar funcion (vuelve a la siguiente linea de la que empezo)
 
-    sub w4, w4, 0x40 			//Reto una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
+    sub w4, w4, 0x40 						//Reto una unidad al color sin alterar los bits del resto de colores ni al bit menos significativo del verde
 
-    cbz w4, alTreinta	    // Si G es su minimo termina la funcion (vuelve a la siguiente linea de la que empezo)
+    cbz w4, alTreinta	   					// Si G es su minimo termina la funcion (vuelve a la siguiente linea de la que empezo)
 
     b bajarVerde //Llamada recirsiva, todavia no termino el color
 
